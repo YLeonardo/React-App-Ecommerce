099fe42ef7d8b373b4430d7c4c9ce5e8f59d6c56
YLeonardo <leonardguti10@gmail.com>
2023-01-15 21:21:18 -0600

    Realiza consulta

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Portada from "images/Jordan3.png";
+import Portada from "images/dunks-panda.png";
 
 export default function Inicio() {
   return (
